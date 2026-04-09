c7c04ac276244ce046bade4334914d8d65be3a8f
Ray <rrcaddick@gmail.com>
2026-04-09 20:02:25 +0200

    Removed Tina Generated from source control

--- .tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:3000/api/tina/gql', token: '', queries,  });
-export default client;
-  
